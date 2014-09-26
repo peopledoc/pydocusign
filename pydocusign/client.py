d2aa3eff58be2f8b4ccf0f94d1ecc5ca3d9fdb15
Benoît Bryon <benoit@marmelune.net>
2014-09-26 15:36:00 +0200

    Refs #12 - DocuSignClient.get_envelope_document() returns a file-like object with a close() method.

--- pydocusign/client.py
+++ pydocusign/client.py
@@ -243,4 +243,5 @@ class DocuSignClient(object):
                       documentId=documentId)
         headers = self.base_headers()
         response = requests.get(url, headers=headers, stream=True)
+        setattr(response.raw, 'close', response.close)
         return response.raw
